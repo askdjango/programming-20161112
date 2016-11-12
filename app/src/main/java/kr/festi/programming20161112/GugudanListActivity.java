e898f64bf4d2f354f7be03669f964c91011e4f7b
Chinseok Lee <allieuslee@gmail.com>
2016-11-12 10:39:42 +0900

    string-array 를 통한 구구단 리스트뷰 등록

--- app/src/main/java/kr/festi/programming20161112/GugudanListActivity.java
+++ app/src/main/java/kr/festi/programming20161112/GugudanListActivity.java
@@ -2,6 +2,10 @@ package kr.festi.programming20161112;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class GugudanListActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class GugudanListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gugudan_list);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                String name = (String) adapterView.getAdapter().getItem(position);
+                Toast.makeText(view.getContext(), name, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/kr/festi/programming20161112/MainActivity.java
+++ app/src/main/java/kr/festi/programming20161112/MainActivity.java
@@ -21,6 +21,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 }
